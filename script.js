7ad09c5136a3600316b2f16b5aaec2c53687d302
Pranav R <59475803+freekpranav@users.noreply.github.com>
2023-12-31 03:37:22 +0000

    Initial commit-2

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const editor = document.getElementById('editor');
+function runCode() {
+    const code = document.getElementById('editor').value;
+    const outputDiv = document.getElementById('output');
 
-    editor.addEventListener('input', function () {
-        // Handle text changes here
-        const content = editor.innerHTML;
-        console.log(content);
-    });
-});
+    try {
+        // Simulate execution with a timeout
+        setTimeout(() => {
+            const result = executeCode(code);
+            outputDiv.innerText = result;
+        }, 500);
+    } catch (error) {
+        outputDiv.innerText = `Error: ${error.message}`;
+    }
+}
+
+function executeCode(code) {
+    // In a real environment, you would execute the code here.
+    // For simplicity, let's just return the code itself.
+    return code;
+}
